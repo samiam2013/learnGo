7a8b41ffd44c58b2c830f66a9a14ccb66cbcc00e
Sam <samiam2013@gmail.com>
2020-10-07 21:17:48 -0500

    I'm really not sure this one is right.

---
+++ elementary/exercise11.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Write a program that computes the sum of an alternating series
+//  where each element of the series is an expression of the form
+//  ((-1)^{k+1})/(2 * k-1)
+// for each value of k from 1 to a million, multiplied by 4.
+func main() {
+	sum := float64(0)
+	for k := float64(1); k <= 1000000; k++ {
+		value := (math.Pow((-1), ((k * 4) + 1))) / (2*(k*4) - 1)
+		sum += value
+		//fmt.Println("sum: ", sum, value)
+	}
+	fmt.Println("sum: ", sum)
+}
