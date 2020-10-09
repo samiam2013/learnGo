cb7be4e5e6d4075d5b592719f2c7a9d9c926c688
Sam <samiam2013@gmail.com>
2020-10-08 23:49:41 -0500

    can't decide between 1) reflect->unsafe->force type or 2) redefine list.

--- Lists_And_Strings/exercise2.go
+++ Lists_And_Strings/exercise2.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
 	"reflect"
+	"unsafe"
 )
+
+type element struct {
+	Next, Prev *element
+	list       *list
+	Value      interface{}
+}
+type list struct {
+	root element // sentinel list element, only &root, root.prev, and root.next are used
+	len  int     // current list length excluding (this) sentinel element
+}
 
 // Write function that reverses a list, preferably in place.
 func main() {
-	strings := list.New()
+	root := element{nil, nil, nil, "root"}
+	strings := list{root, 1}
 	strSlice := []string{"These", "values", "to", "be", "loaded", "into", "list"}
 	for i := 0; i < len(strSlice); i++ {
-		strings.PushBack(strSlice[i])
+
 	}
 	fmt.Println("list forwards: ")
 	printList(strings)
@@ -25,12 +36,15 @@ func printList(l *list.List) {
 	}
 }
 
+// I'll be damned if I can't reverse this in-place with reflection.
 func reverseList(l *list.List) {
-	var elementBuffer *list.Element
 	for e := l.Front(); e != nil; e = e.Next() {
 		// first element previous will be empty
-		elementBuffer = reflect.ValueOf(e).Interface().(*list.Element)
-		elementBuffer.next
+		re := reflect.ValueOf(&e).Elem()
+		reF := re.FieldByName("next")
+		// haha unsafe go brrrrrr
+		ele := reflect.NewAt(reF.Type(), unsafe.Pointer(reF.UnsafeAddr())).Elem()
+		fmt.Println(ele)
 		//elementBuffer = e.next
 		//e.next = e.prev
 		//e.prev = elementBuffer
