b5d65e7b8d46521da13ecfceedcce866b877d781
Sam <samiam2013@gmail.com>
2020-10-11 20:39:50 -0500

    palindromes are easy to find in golang.

---
+++ Lists_And_Strings/exercise6.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// Write a function that tests whether a string is a palindrome.
+func main() {
+	palindrome := "racecar"
+	nonPalindrome := "nascar"
+	printCheck(palindrome)
+	printCheck(nonPalindrome)
+}
+
+func checkPalindrome(candidate string) bool {
+	startIndex := 0
+	endIndex := len(candidate) - 1
+	for i := 0; i <= len(candidate)/2; i++ {
+		if candidate[startIndex+i] != candidate[endIndex-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func printCheck(candidate string) {
+	if checkPalindrome(candidate) {
+		fmt.Println(candidate, "is a palindrome!")
+	} else {
+		fmt.Println(candidate, "is not a palindrome!")
+	}
+}
