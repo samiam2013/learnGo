5f9eac222d84045185c3834350be6002a9bc658c
Sam <samiam2013@gmail.com>
2020-10-14 21:47:20 -0500

    insertion sort done.

--- Lists_And_Strings/exercise17.go
+++ Lists_And_Strings/exercise17.go
@@ -13,6 +13,25 @@ func main() {
 	slice := []int{10, 72, 2, 820, -71, 492, 7, 20138, 7232}
 	selectionSort(slice)
 	fmt.Println(slice)
+	slice = []int{4, 72, 2, 820, -71, 492, 7, 20138, 7232}
+	insertionSort(slice)
+	fmt.Println(slice)
+}
+
+func mergeSort(slice []int) {
+
+}
+
+func insertionSort(slice []int) {
+	for i := 0; i < len(slice); i++ {
+		for j := len(slice) - 1; j > 0; j-- {
+			if slice[j] < slice[j-1] {
+				buf := slice[j]
+				slice[j] = slice[j-1]
+				slice[j-1] = buf
+			}
+		}
+	}
 }
 
 func selectionSort(slice []int) {
