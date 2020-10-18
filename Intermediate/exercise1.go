03b5b62171a3c1b4c0670c783209326e6d5c6b7f
Sam <samiam2013@gmail.com>
2020-10-17 23:06:27 -0500

    using main.go in this folder to call all other files so atom debug works

---
+++ Intermediate/exercise1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Write a program that outputs all possibilities to put + or - or nothing
+// between the numbers 1,2,…,9 (in this order) such that the result is 100.
+// For example 1 + 2 + 3 - 4 + 5 + 6 + 78 + 9 = 100.
+func exercise1() {
+	fmt.Println("this is going to be difficult.")
+}
---
+++ Intermediate/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("running exercises")
+	exercise1()
+}
