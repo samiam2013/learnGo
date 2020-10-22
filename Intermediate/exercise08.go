5b830b60cd0209a76b86d7cf1de009859f7029d0
Sam <samiam2013@gmail.com>
2020-10-21 22:58:10 -0500

    interface

--- Intermediate/exercise08.go
+++ Intermediate/exercise08.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
-//
+// List interface enforces basic functionality for type List
+type List interface {
+	New() *List
+	Len() int
+	Front() *Element
+	Back() *Element
+	PushBack(interface{}) *Element
+	PushFront(interface{}) *Element
+	Remove(*Element) interface{}
+}
+
+// Element interface enforces getters for hidden pointers
+type Element interface {
+	Next() *Element
+	Prev() *Element
+}
+
+// Think of a good interface for a list. What operations do you typically need?
+// You might want to investigate the list interface in your language and in some
+//  other popular languages for inspiration.
 func exercise08() {
-	fmt.Println("what is it this time? ")
+	fmt.Println("this file just implements interfaces for the next exercise.")
 }
