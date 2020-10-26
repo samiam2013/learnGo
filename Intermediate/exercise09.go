731d4eacbf684c621625e38a2328e206017919cf
Sam <samiam2013@gmail.com>
2020-10-25 21:31:20 -0500

    Go's native list with root and ring architecture is far superior.

--- Intermediate/exercise09.go
+++ Intermediate/exercise09.go
@@ -20,7 +20,8 @@ func (l *List) Back() *Element {
 
 // Len implements requirment
 func (l *List) Len() int {
-	return l.last - l.first
+	// at first == last == 0, 1 element is used.
+	return l.last - l.first + 1
 }
 
 // PushBack implements requirment
@@ -50,8 +51,10 @@ func (l *List) PushFront(v interface{}) *Element {
 			l.listSlice[i] = buf
 		}
 		l.listSlice[l.first] = &Element{v, i + 1, l}
-		if buf != nil {
-			l.last++
+		l.last++
+		// this is a hack and I don't know why I need it.
+		if l.listSlice[l.last] == nil {
+			l.last--
 		}
 		return l.listSlice[l.first]
 	}
@@ -97,11 +100,17 @@ func (e *Element) Prev() *Element {
 //  memory you should produce some kind of error, for example you can throw an
 //  exception if your language supports that.
 func exercise09() {
+	// my list implementation is very manual
+	//  and I can't keep go from panicking if you use PushFront()
+	//  for the first element, so I hacked it to check
+	//   if l.listSlice[l.last] == nil and decrement if it is.
 	var l ListI
 	lSlice := make([]*Element, 100)
 	l = &List{lSlice, 0, 0}
-	l.PushBack("World")
+
 	l.PushFront("Hello")
+	l.PushBack("World")
+
 	fmt.Println(l.Front().Value)
 	fmt.Println(l.Back().Value)
 }
