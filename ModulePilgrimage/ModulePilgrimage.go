4626418f5775eee50f8146ca2f1a28aab370aab7
Sam <samiam2013@gmail.com>
2021-02-24 20:31:06 -0600

    come!

---
+++ ModulePilgrimage/ModulePilgrimage.go
@@ -0,0 +1,13 @@
+package ModulePilgrimage
+
+// FizzBuzz does the fizzbuzz thing.
+func FizzBuzz(input int) string {
+	var output string
+	if input%3 == 0 {
+		output = output + "fizz"
+	}
+	if input%5 == 0 {
+		output = output + "buzz"
+	}
+	return output
+}
