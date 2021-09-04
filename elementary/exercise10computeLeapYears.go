c830acf801b8f7aaa19bc0c1f3240f9c7e7d0e8c
Sam Myres <samiam2013@gmail.com>
2021-09-04 19:56:31 +0000

    fixed ex10, sped up anagram

--- elementary/exercise10computeLeapYears.go
+++ elementary/exercise10computeLeapYears.go
@@ -6,6 +6,9 @@ import "fmt"
 func exercise10() {
 	leapYear := 2024
 	for i := 0; i < 20; i++ {
+		if leapYear % 100 == 0 && leapYear % 400 != 0 {
+			leapYear += 4
+		}
 		fmt.Println(leapYear, "is a leap year.")
 		leapYear += 4
 	}
