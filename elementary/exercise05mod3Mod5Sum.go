6819b43aa0c96cb81aab7f2b94b81620db24768a
Sam Myres <samiam2013@gmail.com>
2021-09-07 03:24:16 +0000

    add test case for input 10

--- elementary/exercise05mod3Mod5Sum.go
+++ elementary/exercise05mod3Mod5Sum.go
@@ -6,7 +6,9 @@ import (
 	"strconv"
 )
 
-func exercise05() {
+// FizzBuzzSum : 5. Modify the previous program such that only multiples of three or five
+//	are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
+func FizzBuzzSum() {
 	var inputString string
 	fmt.Print("Input a nubmer: ")
 	fmt.Scanln(&inputString)
