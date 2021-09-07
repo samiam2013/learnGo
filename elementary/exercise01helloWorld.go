37d83aa07a3d9304d935cf39993837bc205d0450
Sam Myres <samiam2013@gmail.com>
2021-09-07 03:57:38 +0000

    remove superflous function

--- elementary/exercise01helloWorld.go
+++ elementary/exercise01helloWorld.go
@@ -2,11 +2,6 @@ package elementary
 
 import "fmt"
 
-// Ex01 exports HelloWorld redundantly. Sorry.
-func Ex01() {
-	HelloWorld()
-}
-
 // HelloWorld : 1. Write a program that prints ‘Hello World’ to the screen.
 func HelloWorld() {
 	fmt.Println("Hello World!")
--- elementary/exercise02yourNameGreeting.go
+++ elementary/exercise02yourNameGreeting.go
@@ -2,11 +2,6 @@ package elementary
 
 import "fmt"
 
-// Ex02 exports GreetName redundantly. sorry.
-func Ex02() {
-	GreetName()
-}
-
 // GreetName : 2. Write a program that asks the user for their name and greets them with their name.
 func GreetName() {
 	var name string
