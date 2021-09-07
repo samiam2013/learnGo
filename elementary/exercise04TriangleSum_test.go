f6f6e4a728c591ac41e9458a6ca69085a7345179
Sam Myres <samiam2013@gmail.com>
2021-09-07 03:58:31 +0000

    add matching single quote

--- elementary/exercise04TriangleSum_test.go
+++ elementary/exercise04TriangleSum_test.go
@@ -10,11 +10,11 @@ import (
 func TestTriangleSum(t *testing.T) {
 	got := testutils.CaptureOutput(TriangleSum, "10\n")
 	if got != "Pick a number, an number that doesn't overflow, of course: 10?: 55\n" {
-		t.Errorf("TriangleSum() = '%s'; want 'Pick a number, an number that doesn't overflow, of course: 10?: 55\\n", got)
+		t.Errorf("TriangleSum() = '%s'; want 'Pick a number, an number that doesn't overflow, of course: 10?: 55\\n'", got)
 	}
 
 	got = testutils.CaptureOutput(TriangleSum, "123456789\n")
 	if got != "Pick a number, an number that doesn't overflow, of course: 123456789?: 7620789436823655\n" {
-		t.Errorf("TriangleSum() = '%s'; want 'Pick a number, an number that doesn't overflow, of course: 10?: 55\\n", got)
+		t.Errorf("TriangleSum() = '%s'; want 'Pick a number, an number that doesn't overflow, of course: 10?: 55\\n'", got)
 	}
 }
