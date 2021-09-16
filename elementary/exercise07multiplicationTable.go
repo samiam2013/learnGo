83c621f987a76dff9cc464ce175f6aa3ad804e85
Sam Myres <samiam2013@gmail.com>
2021-09-16 05:40:36 +0000

    add comment

--- elementary/exercise07multiplicationTable.go
+++ elementary/exercise07multiplicationTable.go
@@ -5,6 +5,8 @@ import (
 )
 
 // Write a program that prints a multiplication table for numbers up to 12
+
+// MultiplicationTable prints a 12 by 12 left-padded mult. table
 func MultiplicationTable() {
 	for row := 1; row <= 12; row++ {
 		for column := 1; column <= 12; column++ {
