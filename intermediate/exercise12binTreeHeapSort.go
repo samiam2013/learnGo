cb18ce8c041140bbe362221271a9e05ec6da248a
Sam Myres <samiam2013@gmail.com>
2021-09-17 01:24:19 +0000

    heapify is working. better not get ahead of myself

--- intermediate/exercise12binTreeHeapSort.go
+++ intermediate/exercise12binTreeHeapSort.go
@@ -2,6 +2,7 @@ package intermediate
 
 import (
 	"fmt"
+	"log"
 	"strings"
 )
 
@@ -20,31 +21,28 @@ type binTreeNode struct {
 func BinTreeHeapSort() {
 	fmt.Println("exercise 12...")
 
-	tree := &binTree{
-		&binTreeNode{15,
-			&binTreeNode{8,
-				&binTreeNode{3,
-					nil,
-					nil,
-				},
-				&binTreeNode{1,
-					nil,
-					nil,
-				},
-			}, &binTreeNode{5,
-				&binTreeNode{2,
-					nil,
-					nil,
-				},
-				nil,
+	tree := &binTree{&binTreeNode{1,
+		&binTreeNode{6,
+			&binTreeNode{13,
+				&binTreeNode{14, nil, nil},
+				&binTreeNode{12, nil, nil},
 			},
-		},
-	}
+			&binTreeNode{11, nil, nil},
+		}, &binTreeNode{54,
+			&binTreeNode{2,
+				&binTreeNode{16, nil, nil},
+				&binTreeNode{11, nil, nil},
+			},
+			&binTreeNode{13, nil, nil},
+		}}}
 
 	recursiveDFSPrintTree(tree.root)
 	fmt.Println() // recursive print doesn't leave a newline TODO fix?
 	fmt.Println("number of levels: ", countLeftMostLevels(tree.root, 0))
-	prettyPrintTree(tree)
+	prettyPrintTree(tree.root)
+	parentStack := []*binTreeNode{tree.root, tree.root.left}
+	heapify(tree.root.left.left, parentStack)
+	prettyPrintTree(tree.root)
 }
 
 func recursiveDFSPrintTree(node *binTreeNode) {
@@ -65,11 +63,11 @@ func countLeftMostLevels(node *binTreeNode, levels int) int {
 	return levels
 }
 
-func prettyPrintTree(tree *binTree) {
-	numLevels := countLeftMostLevels(tree.root, 0)
+func prettyPrintTree(root *binTreeNode) {
+	numLevels := countLeftMostLevels(root, 0)
 	curLevel := 0
 	levelNodes := make(map[int][]*binTreeNode)
-	levelNodes[0] = append(levelNodes[0], tree.root)
+	levelNodes[0] = append(levelNodes[0], root)
 	// going over each level print the value and check for values to append from below, left to right
 	for curLevel < numLevels {
 		// figure out how much padding to print here
@@ -89,4 +87,53 @@ func prettyPrintTree(tree *binTree) {
 		// have to increment the current level
 		curLevel = curLevel + 1
 	}
+}
+
+func heapify(node *binTreeNode, parentStack []*binTreeNode) {
+	largestNode := node
+	// if either child is larger than largest so far
+	//fmt.Println("root val ", node.value)
+	if node.left != nil && node.left.value > largestNode.value {
+		largestNode = node.left
+	}
+	if node.right != nil && node.right.value > largestNode.value {
+		largestNode = node.right
+	}
+	if largestNode != node {
+		// fmt.Printf("change left '%d' with largest node '%d'\n",
+		// 	node.value, largestNode.value)
+		buf := node.value
+		node.value = largestNode.value
+		largestNode.value = buf
+		// fmt.Printf("after change left '%d' with largest node '%d'\n",
+		// 	node.value, largestNode.value)
+		// prettyPrintTree(largestNode)
+		if len(parentStack) > 0 {
+			heapify(parentStack[len(parentStack)-1], parentStack[:len(parentStack)-1])
+		}
+	}
+}
+
+func nextNodeParent(root *binTreeNode) *binTreeNode {
+	numLevels := countLeftMostLevels(root, 0)
+	curLevel := 0
+	levelNodes := make(map[int][]*binTreeNode)
+	levelNodes[0] = append(levelNodes[0], root)
+	// going over each level print the value and check for values to append from below, left to right
+	for curLevel < numLevels {
+		// figure out how much padding to print here
+		for i := 0; i < len(levelNodes[curLevel]); i++ {
+			curNode := levelNodes[curLevel][i]
+			//fmt.Printf(" %3d", curNode.value)
+			if curNode.left != nil || curNode.right != nil {
+				return curNode
+			}
+		}
+		// print newline after each level
+		fmt.Println()
+		// have to increment the current level
+		curLevel = curLevel + 1
+	}
+	log.Fatalln("could not get a new node, no fix in sight")
+	return nil
 }
