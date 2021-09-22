cfd26fdbf12bad00739c2bb85bbb1ab778824f56
Samuel Myres <samiam2013@gmail.com>
2021-09-21 19:11:18 -0500

    remove dependency

--- elementary/exercise01helloWorld.go
+++ elementary/exercise01helloWorld.go
@@ -1,8 +1,6 @@
 package elementary
 
-import "fmt"
-
 // HelloWorld : 1. Write a program that prints ‘Hello World’ to the screen.
 func HelloWorld() {
-	fmt.Println("Hello World!")
+	print"Hello World!"\n)
 }
