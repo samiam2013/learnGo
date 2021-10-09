61d17d65ca7dcd84d2c40b4996e2ae2b15c53cc6
Samuel <samiam2013@gmail.com>
2021-10-09 21:28:48 +0000

    golint, misspell

---
+++ offscript/runSudoku.go
@@ -0,0 +1,36 @@
+package offscript
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+// RunSudoku is used for calling from ../main.go to test things by hand
+func RunSudoku() error {
+	board := board{
+		Cells: [9][9]*cell{},
+	}
+	err := board.Ingest(hiddenSinglesBoard)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if false {
+		zeros := map[string]interface{}{
+			"row0": getRow(&board, 0),
+			"col0": getCol(&board, 0),
+			"set0": getSet(&board, 0)}
+		marshalled, _ := json.MarshalIndent(zeros, "", "  ")
+		fmt.Printf("%s\n", string(marshalled))
+	}
+
+	board.Print()
+	fmt.Printf("is board valid?: %v\n", board.IsValid())
+	solveHiddenSingles(&board)
+	board.Print()
+	fmt.Printf("is board valid?: %v\n", board.IsValid())
+
+	return err
+
+}
