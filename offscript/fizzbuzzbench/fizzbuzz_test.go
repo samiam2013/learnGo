51fa02438c7269d5362e4e5d1199c493065209b6
Sam Myres <samiam2013@gmail.com>
2023-07-27 00:49:19 -0500

    remove ineffective CPU affinity conflicting with macos

--- offscript/fizzbuzzbench/fizzbuzz_test.go
+++ offscript/fizzbuzzbench/fizzbuzz_test.go
@@ -2,15 +2,7 @@ package main
 
 import (
 	"testing"
-
-	"golang.org/x/sys/unix"
 )
-
-func init() {
-	var set unix.CPUSet
-	_ = unix.SchedGetaffinity(0, &set)
-	_ = unix.SchedSetaffinity(0, &set)
-}
 
 func BenchmarkFizzBuzzModulo(b *testing.B) {
 	fizzBuzzModulo(b.N)
